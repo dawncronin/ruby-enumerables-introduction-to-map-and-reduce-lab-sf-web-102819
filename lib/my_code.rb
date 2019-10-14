a2bda1b314f16f1cec804a4cd5451d301a179e2c
Dawn Cronin <dawncronin7@gmail.com>
2019-10-14 02:44:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,7 +22,12 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  
+  flag = true
+  source_array.each do |ele|
+    if ele != true 
+      flag = flase
+    end
+    return flag
 end
 
 def reduce_to_any_false(source_array)
